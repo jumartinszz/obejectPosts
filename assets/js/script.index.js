a20d7311489298ff08742b980656c03f239a769b
julia martins <juliamartinss.564@gmail.com>
2023-08-07 15:01:47 -0300

    feat: create edit and remove posts functions

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,4 +1,5 @@
 const posts = [];
+let indexPost = -1;
 
 function savePost() {
     const title = document.getElementById("title").value;
@@ -8,8 +9,24 @@ function savePost() {
 
     console.log(title, resume, publisher, date)
 
-    if (title && resume && publisher && date) {
-        storePost(title, resume, publisher, date)
+    if (indexPost == -1) {
+        if (title && resume && publisher && date) {
+            storePost(title, resume, publisher, date);
+            cleanFields();
+        }
+    } else {
+        if (title && resume && publisher && date){
+            posts[indexPost] = {
+                title,
+                resume,
+                publisher,
+                date
+            };
+
+            showPosts();
+            indexPost = -1;
+            cleanFields();
+        }
     }
 }
 
@@ -46,4 +63,28 @@ function showPosts() {
     })
 
     document.getElementById("list").innerHTML = showContent;
+}
+
+function cleanFields(){
+    document.getElementById("title").value = "";
+    document.getElementById("publisher").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("resume").value = "";
+}
+
+function editPost(index){
+    indexPost = index;
+
+    const post = posts[index];
+
+    document.getElementById("title").value = post.title;
+    document.getElementById("resume").value = post.resume;
+    document.getElementById("publisher").value = post.publisher;
+    document.getElementById("date").value = post.date;
+}
+
+function removePost(index){
+    posts.splice(index, 1);
+
+    showPosts();
 }
